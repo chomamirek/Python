0a785632c4f4cadc2df90a313a3991d4f74f255c
Mirek <35068320+chomamirek@users.noreply.github.com>
2019-04-22 12:02:40 +0200

    1
    1

---
+++ Untitled-3.py
@@ -0,0 +1,12 @@
+#%%
+napis = witaj
+rzeczywista = 110.0
+calkowita = 20
+
+# sprawdzenie kodu
+if napis == "witaj":
+    print ("Napis: %s" % napis)
+if isinstance(rzeczywista, float) and rzeczywista == 10.0:
+    print ("Liczba rzeczywista: %d" % rzeczywista)
+if isinstance(calkowita, int) and calkowita == 20:
+    print ("Liczba calkowita: %d" % calkowita)
---
+++ Untitled-4.py
@@ -0,0 +1,19 @@
+#%%
+liczby = []
+napisy = []
+imiona = ["Jan", "Edward", "Joanna"]
+
+# Tu umiesc swoj kod
+drugie_imie = imiona[1]
+liczby.append(1)
+liczby.append(2)
+liczby.append(3)
+napisy.append("Witaj")
+napisy.append("swiecie")
+
+
+# Ten fragment kodu wypisze wypelnione przez ciebie
+# tablice oraz drugie imie w tablicy imion (Edward).
+print (liczby)
+print (napisy)
+print ("Drugim imieniem w tablicy jest %s." % drugie_imie)
